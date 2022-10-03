427a068532e2ae667833ba40e19c3d93da9b1269
juanjguanipaf@gmail.com <juanjguanipaf@gmail.com>
2022-10-03 17:49:12 -0400

    done

--- index.js
+++ index.js
@@ -17,6 +17,35 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Pabellon",
+      level: "Anyone",
+      ingredients: [
+        "Rice",
+        "beef",
+        "black beans"
+      ],
+      cuisine: "Venezuelan",
+      dishType: "main_course",
+      duration: 50,
+      creator: "a Venezuelan guy"
+    })
+      .then(created => {
+        console.log(created.title)
+        return Recipe.insertMany(data)
+          .then((created) => {
+            console.log(created)
+            return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+              .then((recipe) => {
+                console.log('Rigatoni alla Genovese was updated')
+                return Recipe.deleteOne({ title: "Carrot Cake" })
+                  .then(() => {
+                    console.log('Carrot Cake was deleted')
+                  })
+              })
+          })
+      })
+      .catch(err => console.log(err))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,7 +3,30 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  level: String,
+  ingridients: [String],
+  cuisine: String,
+  dishType: String,
+  images: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
